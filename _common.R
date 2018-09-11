1630ced2f5e3b97bcbc528a3f5d81f8434e07831
achambaz <antoine.chambaz@parisdescartes.fr>
2018-09-11 09:36:23 +0200

    Adding config files

---
+++ _common.R
@@ -0,0 +1,19 @@
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6, # used to have: fig.width = 12,
+  fig.asp = 0.618, # used to have: fig.height = 4, 
+  fig.show = "hold",
+  fig.path = 'img/',
+  size = "tiny",
+  message = FALSE,
+  warning = FALSE,
+  warnings = FALSE
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
